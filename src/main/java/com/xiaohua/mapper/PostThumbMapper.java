00cac97a03917798012f5df750f6ac3195a415c8
hanFei <2035346428@qq.com>
2024-12-12 21:40:26 +0800

    重构项目

---
+++ src/main/java/com/xiaohua/mapper/PostThumbMapper.java
@@ -0,0 +1,18 @@
+package com.xiaohua.mapper;
+
+import com.xiaohua.model.entity.PostThumb;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * 帖子点赞数据库操作
+ *
+ * @author 小花
+ * @from 好好学习
+ */
+public interface PostThumbMapper extends BaseMapper<PostThumb> {
+
+}
+
+
+
+
