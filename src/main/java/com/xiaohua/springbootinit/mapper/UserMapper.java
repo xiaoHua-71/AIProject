a3379bae0b7818c41cdde5c5dda1a08216713910
hanFei <2035346428@qq.com>
2024-12-12 20:34:17 +0800

    开始完善项目

---
+++ src/main/java/com/xiaohua/springbootinit/mapper/UserMapper.java
@@ -0,0 +1,18 @@
+package com.xiaohua.springbootinit.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.xiaohua.springbootinit.model.entity.User;
+
+/**
+ * 用户数据库操作
+ *
+ * @author 小花
+ * @from 好好学习
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
